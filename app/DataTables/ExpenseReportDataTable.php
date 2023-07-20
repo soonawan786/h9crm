0a040bb6cb56929dbfdc1548a16ecc56b23b2424
baqar1 <baqarcit@gmail.com>
2023-07-20 17:19:36 +0500

    'bankstatusUpdate'

--- app/DataTables/ExpenseReportDataTable.php
+++ app/DataTables/ExpenseReportDataTable.php
@@ -25,6 +25,9 @@ class ExpenseReportDataTable extends BaseDataTable
     {
         return datatables()
             ->eloquent($query)
+            ->addColumn('bank_name', function (Expense $expense) {
+                return $expense->bankAccount ? $expense->bankAccount->bank_name : 'Cash';
+            })
             ->addColumn('check', function ($row) {
                 return '<input type="checkbox" class="select-table-row" id="datatable-row-' . $row->id . '"  name="datatable_ids[]" value="' . $row->id . '" onclick="dataTableRowCheck(' . $row->id . ')">';
             })
@@ -90,8 +93,8 @@ class ExpenseReportDataTable extends BaseDataTable
     public function query()
     {
         $request = $this->request();
-        $model = Expense::with('currency', 'user', 'user.employeeDetail', 'user.employeeDetail.designation', 'user.session')
-            ->select('expenses.id', 'expenses.item_name', 'expenses.user_id', 'expenses.price', 'users.name', 'expenses.purchase_date', 'expenses.currency_id', 'currencies.currency_symbol', 'expenses.status', 'expenses.purchase_from', 'expenses.expenses_recurring_id', 'designations.name as designation_name', 'expenses.added_by')
+        $model = Expense::with('currency', 'user', 'user.employeeDetail', 'user.employeeDetail.designation', 'user.session','bankAccount')
+            ->select('expenses.id', 'expenses.item_name', 'expenses.user_id', 'expenses.price', 'users.name', 'expenses.purchase_date', 'expenses.currency_id', 'currencies.currency_symbol', 'expenses.status', 'expenses.purchase_from', 'expenses.expenses_recurring_id', 'designations.name as designation_name', 'expenses.added_by','expenses.bank_account_id')
             ->join('users', 'users.id', 'expenses.user_id')
             ->leftJoin('employee_details', 'employee_details.user_id', '=', 'users.id')
             ->leftJoin('designations', 'employee_details.designation_id', '=', 'designations.id')
@@ -171,6 +174,7 @@ class ExpenseReportDataTable extends BaseDataTable
             __('app.employee') => ['data' => 'employee_name', 'name' => 'user_id', 'visible' => false, 'title' => __('app.employee')],
             __('modules.expenses.purchaseFrom') => ['data' => 'purchase_from', 'name' => 'purchase_from', 'title' => __('modules.expenses.purchaseFrom')],
             __('modules.expenses.purchaseDate') => ['data' => 'purchase_date', 'name' => 'purchase_date', 'title' => __('modules.expenses.purchaseDate')],
+            __('modules.expenses.bankName') => ['data' => 'bank_name', 'name' => 'bank_name', 'title' => __('modules.expenses.bankName')],
             __('app.status') => ['data' => 'status', 'name' => 'status', 'exportable' => false, 'title' => __('app.status')],
             __('app.status') . ' ' . __('app.status') => ['data' => 'status', 'name' => 'status', 'visible' => false, 'title' => __('app.expense')]
         ];
